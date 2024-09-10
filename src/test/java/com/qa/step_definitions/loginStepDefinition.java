065181679f2a07266f4e61a6e9cb910c110398ef
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-10 15:54:21 +0200

    Create step definitions templates

---
+++ src/test/java/com/qa/step_definitions/loginStepDefinition.java
@@ -0,0 +1,37 @@
+package com.qa.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class loginStepDefinition {
+    @Given("User should be on login page")
+    public void userShouldBeOnLoginPage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Enter username as {string}")
+    public void enterUsernameAs(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Enter password as {string}")
+    public void enterPasswordAs(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Press login button")
+    public void pressLoginButton() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Login fails with error Username and password do not match any user in this service.")
+    public void loginFailsWithErrorUsernameAndPasswordDoNotMatchAnyUserInThisService() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Products Page opens with title {string}")
+    public void productsPageOpensWithTitle(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/java/com/qa/step_definitions/productsStepDefinition.java
@@ -0,0 +1,28 @@
+package com.qa.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class productsStepDefinition {
+    @Given("User should be logged in")
+    public void userShouldBeLoggedIn() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Click product title {string}")
+    public void clickProductTitle(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Products Page opens with title {string} and price {string}")
+    public void productsPageOpensWithTitleAndPrice(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Product Details Page opens with title {string}, price {string} and description {string}")
+    public void productDetailsPageOpensWithTitlePriceAndDescription(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
