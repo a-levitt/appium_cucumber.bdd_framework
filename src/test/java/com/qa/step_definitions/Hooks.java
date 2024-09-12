6d61a87122fe4ddc82e2c2b4f589e5e0c9e81fe4
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-12 13:03:41 +0200

    Set hooks to create and close drivers

--- src/test/java/com/qa/step_definitions/Hooks.java
+++ src/test/java/com/qa/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.qa.step_definitions;
 
+import com.qa.utils.DriverManager;
 import com.qa.utils.GlobalParams;
 import com.qa.utils.ServerManager;
 import io.cucumber.java.*;
@@ -8,13 +9,14 @@ import org.apache.logging.log4j.ThreadContext;
 public class Hooks {
 
     @Before
-    public void initialize() {
+    public void initialize() throws Exception {
         GlobalParams params = new GlobalParams();
         params.initializeGlobalParams();
 
         ThreadContext.put("ROUTINGKEY", params.getPlatformName() + "_" + params.getDeviceName());
 
         new ServerManager().startServer();
+        new DriverManager().initializeDriver();
     }
 
     @After
@@ -22,6 +24,15 @@ public class Hooks {
         ServerManager serverManager = new ServerManager();
         if (serverManager.getServer() != null) {
             serverManager.getServer().stop();
+        }
+        DriverManager driverManager = new DriverManager();
+        if (driverManager != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+        if (driverManager != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
         }
     }
 
